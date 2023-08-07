dccf9ef67e383dc30c342c70140918cc0d3a1e07
root <root@VM-Dev-Ubux.ewuq5u5vkkge5pkmjdwdgzpelg.ax.internal.cloudapp.net>
2023-08-07 08:58:03 +0000

    first upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Web project Ubux
+a simple webserver deployment 
+
+create two containers and start a nginx webserver
---
+++ app/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ubux Web Server</title>
+</head>
+<body>
+    <h2>Guten Morgen Ubux</h2>
+    </br>
+    <a href="https://www.faz.de">Faz.de</a>
+    </br>
+    <a href="https://www.kicker.de">Kicker.de</a>
+    </br>
+</body>
+</html>
